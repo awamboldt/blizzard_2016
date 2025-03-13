6496d446a130d5b53038c6cdfee805f6b4ef6549
Alexander Wamboldt <alexander.wamboldt@gmail.com>
2025-03-13 12:29:37 +0100

    fixed typos in prep weather

--- models/prep/prep_weather.sql
+++ models/prep/prep_weather.sql
@@ -1,13 +1,13 @@
 WITH merged_snow AS 
-    (SELECT d.airport_code
+    (SELECT d.airport_code AS faa
     , d.date
     , h.time
     , AVG(d.max_snow_mm) AS snow
     FROM {{ref('staging_weather_hourly')}} AS h
     LEFT JOIN {{ref('staging_weather_daily')}} AS d ON (h.date=d.date AND d.airport_code = h.airport_code)
-    GROUP BY d.airport_code, d.date, h.time
+    GROUP BY faa, d.date, h.time
     ORDER BY d.date, h.time)
 SELECT s.*
     , w.*
 FROM merged_snow AS s
-LEFT JOIN {{ref('staging_weather_hourly')}} AS w ON (s.date = w.date AND s.time = w.time AND s.airport_code = w.airport_code)
+LEFT JOIN {{ref('staging_weather_hourly')}} AS w ON (s.date = w.date AND s.time = w.time AND s.faa = w.airport_code)
