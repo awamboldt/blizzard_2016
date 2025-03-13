1166e60c1fa4324ccaef756ecac79451986335c3
Alexander Wamboldt <alexander.wamboldt@gmail.com>
2025-03-13 12:15:53 +0100

    fixing the weather no snow issue

---
+++ models/prep/prep_weather.sql
@@ -0,0 +1,12 @@
+WTIH merged_snow AS (SELECT d.airport_code
+    , d.date
+    , h.time
+    , AVG(d.max_snow_mm) AS snow
+    FROM {{ref('prep_weather_hourly')}} AS h
+    LEFT JOIN {{ref('staging_weather_daily')}} AS d ON (h.date=d.date AND d.airport_code = h.airport_code)
+    GROUP BY d.airport_code, d.date, h.time
+    ORDER BY d.date, h.time)
+SELECT s.*
+    , w.*
+FROM merged_snow AS s
+LEFT JOIN {{ref('staging_weather_hourly')}} AS w ON (s.date = w.date AND s.time = w.time AND s.airport_code = w.airport_code)
