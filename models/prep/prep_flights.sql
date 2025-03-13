27eec6a1545b4976fda3bdbcf8972af1ba8236b1
Alexander Wamboldt <alexander.wamboldt@gmail.com>
2025-03-13 12:58:26 +0100

    cleaned prep tables

--- models/prep/prep_flights.sql
+++ models/prep/prep_flights.sql
@@ -4,26 +4,34 @@ WITH flights_data AS (
 ),
 flights_cleaned AS(
     SELECT flight_date::DATE
-            , TO_CHAR(dep_time, 'fm0000')::TIME AS dep_time
-            , TO_CHAR(sched_dep_time, 'fm0000')::TIME AS sched_dep_time
-            ,dep_delay
-		    ,(dep_delay * '1 minute'::INTERVAL) AS dep_delay_interval
-            ,TO_CHAR(arr_time, 'fm0000')::TIME AS arr_time
-            ,TO_CHAR(sched_arr_time, 'fm0000')::TIME AS sched_arr_time
-            ,arr_delay
-            ,(arr_delay * '1 minute'::INTERVAL) AS arr_delay_interval
-            ,airline
-            ,tail_number
-            ,flight_number
-            ,origin
-            ,dest
-            ,air_time
-            ,(air_time * '1 minute'::INTERVAL) AS air_time_interval
-            ,actual_elapsed_time
-            ,(actual_elapsed_time * '1 minute'::INTERVAL) AS actual_elapsed_time_interval
-            ,(distance / 0.621371)::NUMERIC(6,2) AS distance_km -- see instruction hint
-            ,cancelled
-            ,diverted
-    FROM flights_data
+            , TO_CHAR(f.dep_time, 'fm0000')::TIME AS dep_time
+            , TO_CHAR(f.sched_dep_time, 'fm0000')::TIME AS sched_dep_time
+            , TO_CHAR(f.dep_time,'HH24:MI') as dep_hour  -- time (hours:minutes) as TEXT data type
+            ,f.dep_delay
+		    ,(f.dep_delay * '1 minute'::INTERVAL) AS dep_delay_interval
+            ,TO_CHAR(f.arr_time, 'fm0000')::TIME AS arr_time
+            ,TO_CHAR(f.sched_arr_time, 'fm0000')::TIME AS sched_arr_time
+            , TO_CHAR(f.arr_time,'HH24:MI') as arr_hour  -- time (hours:minutes) as TEXT data type
+            ,f.arr_delay
+            ,(f.arr_delay * '1 minute'::INTERVAL) AS arr_delay_interval
+            ,f.airline
+            ,f.tail_number
+            ,f.flight_number
+            ,f.origin
+            ,air1.city AS origin_city
+            ,air1.name AS origin_name
+            ,f.dest
+            ,air2.city AS dest_city
+            ,air2.name AS dest_name
+            ,f.air_time
+            ,(f.air_time * '1 minute'::INTERVAL) AS air_time_interval
+            ,f.actual_elapsed_time
+            ,(f.actual_elapsed_time * '1 minute'::INTERVAL) AS actual_elapsed_time_interval
+            ,(f.distance / 0.621371)::NUMERIC(6,2) AS distance_km -- see instruction hint
+            ,f.cancelled
+            ,f.diverted
+    FROM flights_data as f
+    LEFT JOIN {{ref('staging_airports')}} AS air1 ON air1.faa = f.origin
+    LEFT JOIN {{ref('staging_airports')}} AS air2 ON air2.faa = f.dest
 )
 SELECT * FROM flights_cleaned
--- models/prep/prep_weather_cleaned.sql
+++ models/prep/prep_weather_cleaned.sql
@@ -1,17 +1,3 @@
-WITH merged_snow AS 
-    (SELECT d.airport_code AS faa
-    , d.date
-    , h.time
-    , AVG(d.max_snow_mm) AS snow
-    FROM {{ref('staging_weather_hourly')}} AS h
-    LEFT JOIN {{ref('staging_weather_daily')}} AS d ON (h.date=d.date AND d.airport_code = h.airport_code)
-    GROUP BY faa, d.date, h.time
-    ORDER BY d.date, h.time),
-added_snow AS 
-    (SELECT s.snow
-    , w.*
-    FROM merged_snow AS s
-    LEFT JOIN {{ref('staging_weather_hourly')}} AS w ON (s.date = w.date AND s.time = w.time AND s.faa = w.airport_code))
 SELECT
 	airport_code
 	, date
@@ -54,4 +40,4 @@ SELECT
 		WHEN condition_code = 27 THEN 'storm'
 		ELSE 'data_missing'
 	END) AS condition	
-FROM added_snow
+FROM {{ref('prep_weather')}}
